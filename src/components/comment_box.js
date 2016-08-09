220786e969f50d1c96356f5564873c7991c99e2b
Jesus Nana <onin@mbp.local>
2016-08-09 23:04:25 +0800

    form add and listing (tdd)

---
+++ src/components/comment_box.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class CommentBox extends Component {
+  constructor(props) {
+      super(props);
+
+      this.state = { comment: ''};
+  }
+
+  handleChange(event) {
+    this.setState({ comment: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.saveComment(this.state.comment);
+    this.setState({ comment: '' });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit.bind(this)} className="comment-box">
+        <h4>Add a comment</h4>
+        <div>
+          <textarea value={this.state.comment} onChange={this.handleChange.bind(this)} />
+        </div>
+        <div>
+          <button action="submit">Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { comments: state.comments }
+}
+
+export default connect(null, actions)(CommentBox);
--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,22 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
+// Use 'describe' to group together similar tests
+describe('App', () => {
   let component;
 
   beforeEach(() => {
     component = renderComponent(App);
   });
 
-  it('renders something', () => {
-    expect(component).to.exist;
+  // Use 'it' to test a single attribute of a target
+  it('shows a comment box', () => {
+    // Use 'expect' to make an 'assertion' about a target
+    expect(component.find('.comment-box')).to.exist;
   });
+
+  it('shows a comment list', () => {
+    expect(component.find('.comment-list')).to.exist;
+  });
+
 });
